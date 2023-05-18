3690ca904f1a14af24ec73c1bf718f6e704b3daa
Ulises <ulyses.sosa@gmail.com>
2023-05-18 22:39:13 +0200

    Add logs to methods,refactor code

--- Resources/trip_resource.py
+++ Resources/trip_resource.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, abort
 from sqlalchemy.orm.exc import NoResultFound
 from Schemas.trip_schema import TripSchema
 from Models.trip import Trip
+from Models.user import User
 from flask import request
 from sqlalchemy.exc import IntegrityError
 from Api.database import db
@@ -13,25 +14,36 @@ TRIP_ENDPOINT = "/api/trip"
 
 class TripResource(Resource):
 
-    def retrieveTripById(self,id):
+    def __retrieveTripById(id):
         trip = Trip.query.filter_by(id= id).first()
         trip_json = TripSchema().dump(trip)
         if not trip_json:
              raise NoResultFound()
-        return trip_json
+        return trip_json, 200
     
-    def get(self, id=None):
+    def get(self, id=None, userId=None):
         """
         TripResource GET method. Retrieves the information related to a trip with the passed id in the request
         """
         if id:
+            logger.info(f"Retrieving trip with id {id}")
             try:
-                self.retrieveTripById(id)
+                self.__retrieveTripById(id)
             except NoResultFound:
+                    logger.error(f"Trip with id {id} not found in database")
                     abort(404, message=f"Trip with id {id} not found in database")
+
         else:
-            trips = Trip.query.all()
-            return [TripSchema().dump(trip) for trip in trips] 
+            logger.info(f"retrieving all Trips in db")
+            try:
+                trips = Trip.query.all()
+                trips_json =  [TripSchema().dump(trip) for trip in trips]
+                if len(trips_json) == 0:
+                    abort(404, message=f"No trips in db")
+                return trips_json, 200
+            except IntegrityError:
+                    abort(500, message=f"Error while retrieving trips")
+        
     def post(self):
         """
         TripResource POST method. Adds a new trip to the database.
