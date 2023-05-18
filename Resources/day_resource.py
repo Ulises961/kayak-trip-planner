648fc025316becbcf868e1402d28e25c0189cf5c
Ulises <ulyses.sosa@gmail.com>
2023-05-18 22:38:55 +0200

    Add logs to methods,refactor code

--- Resources/day_resource.py
+++ Resources/day_resource.py
@@ -6,28 +6,48 @@ from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import IntegrityError
 from Schemas.day_schema import DaySchema
 from Models.day import Day
+from Models.itinerary import Itinerary
 
-logger = logging.getLogger(__name__) # It will print the name of this module when the main app is running
+# It will print the name of this module when the main app is running
+logger = logging.getLogger(__name__)
 
 DAY_ENDPOINT = "/api/day"
 
+
 class DayResource(Resource):
 
-    def retrieveDayById(id):
+    def __retrieveDayById(id):
         day = Day.query.filter_by('id', id).first()
         day_json = DaySchema.dump(day)
+
         if not day_json:
-             raise NoResultFound()
-        return day_json
-    
-    def get(self, id=None):
+            raise NoResultFound()
+        
+        return day_json, 200
+
+    def get(self, id=None, itineraryId=None):
         """
         DayResource GET method. Retrieves the information related to the day with the passed id in the request
         """
-        try:
-            self.retrieveDayById(id)
-        except NoResultFound:
+        if id:
+            logger.info(f"Retrive day with id {id}")
+            
+            try:
+                json = self.__retrieveDayById(id)
+                return json, 200
+            except NoResultFound:
                 abort(404, message=f"Day with id {id} not found in database")
+
+        else:
+            logger.info(f"Retrive all days from db")
+            try:
+                days = Day.query.all()
+                days_json = [DaySchema().dump(day) for day in days]
+                if len(days_json) == 0:
+                    abort(404, message=f"No days in db")
+                return days_json, 200
+            except IntegrityError:
+                abort(500, message=f"Error while retrieving days")
 
     def post(self):
         """
@@ -35,19 +55,19 @@ class DayResource(Resource):
 
         :return: Day, 201 HTTP status code.
         """
+
+        logger.info(f"Insert day {request.get_json()} in db")
         day = DaySchema().load(request.get_json())
 
         try:
             db.session.add(day)
             db.session.commit()
+
         except IntegrityError as e:
             logger.warning(
                 f"Integrity Error, this day is already in the database. Error: {e}"
             )
-
             abort(500, message="Unexpected Error!")
+
         finally:
             return DaySchema().dump(day), 201
-
-
-        
