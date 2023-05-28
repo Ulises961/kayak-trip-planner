1a1b5ebdacd10f7e9b9f0e94945398ceb5fe716a
Balawal Sultan <balawalsultan786@gmail.com>
2023-05-28 18:18:19 +0200

    add failing test for 5_items_resource
    Idk why this test is not working and can't figure it out.

---
+++ Tests/5_items_resource.py
@@ -0,0 +1,26 @@
+from Resources.item_resource import ITEM_ENDPOINT
+import json 
+
+def test_insert_item_unchecked(app):
+    item = {"category": 'generic', "name": 'generic_item'}
+
+    response = app.post(ITEM_ENDPOINT, json=item)
+    assert response.status_code == 201
+
+def test_update_item(app):
+    item = {"id": 55, "category": 'generic', "checked": True, "name": 'generic_item'}
+    
+    response = app.put(ITEM_ENDPOINT, json=item)
+    
+    assert json.loads(response.data)['checked'] == True
+    assert response.status_code == 201
+
+
+def test_get_item_by_id(app):
+    response = app.get(f"{ITEM_ENDPOINT}?id=55")
+    assert json.loads(response.data)['checked'] == True
+    assert response.status_code == 200
+
+def test_delete_item(app):
+    response = app.delete(f"{ITEM_ENDPOINT}?id=55")
+    assert response.status_code == 200
