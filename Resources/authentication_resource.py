c9499511d223ac9c6c56a33386f4f2c1cc8cf71f
Balawal Sultan <balawalsultan786@gmail.com>
2023-07-22 18:22:32 +0200

    implement auth token generation

--- Resources/authentication_resource.py
+++ Resources/authentication_resource.py
@@ -10,12 +10,28 @@ from http import HTTPStatus
 from sqlalchemy import exc, or_
 from Schemas.user_schema import UserSchema
 from Api.database import db
+from functools import wraps
 
 # It will print the name of this module when the main app is running
 
 log = logging.getLogger(__name__)
 api = Blueprint("auth", __name__)
 
+def generate_auth_token(public_id):
+    return jwt.encode(
+        {
+            "public_id": public_id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=45),
+        },
+        current_app.config["SECRET_KEY"],
+        "HS256",
+    )
+    
+
+def decode_auth_token(auth_token):
+    return jwt.decode(
+        auth_token, current_app.config["SECRET_KEY"], ["HS256"]
+    )
 
 @api.route("/api/auth/ping", methods=["GET"])
 @authenticate_restful
@@ -38,37 +54,36 @@ def get_user_status(resp):
 
 @api.route("/api/auth/login", methods=["POST"])
 def login_user():
+    
+    response_object = {"status": "fail", "message": "Invalid payload"}
+    
     post_data = request.get_json()
     log.info(f"Post data {post_data}")
-    response_object = {"status": "fail", "message": "Invalid payload"}
+    
     if not post_data:
         return jsonify(response_object), HTTPStatus.BAD_REQUEST
 
     mail = post_data.get("email")
     password = post_data.get("password")
+    
     try:
         user = User.query.filter_by(mail=mail).first()
+        
         if user and check_password_hash(user.pwd, password):
-            auth_token = jwt.encode(
-                {
-                    "public_id": user.public_id,
-                    "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=45),
-                },
-                current_app.config["SECRET_KEY"],
-                "HS256",
-            )
+            auth_token = generate_auth_token(user["public_id"])
+            
             if auth_token:
                 response_object = {
                     "status": "success",
                     "message": "Successfully logged in",
-                    "auth_token": jwt.decode(
-                        auth_token, current_app.config["SECRET_KEY"], ["HS256"]
-                    ),
+                    "auth_token": auth_token,
                 }
+                
                 return jsonify(response_object), HTTPStatus.OK
         else:
             response_object["message"] = "User does not exist"
             return jsonify(response_object), HTTPStatus.NOT_FOUND
+        
     except Exception as e:
         log.error(e)
         response_object["message"] = "Try again."
@@ -85,39 +100,49 @@ def logout_user(resp):
 
 @api.route("/api/auth/register", methods=["POST"])
 def register_user():
-    post_data = request.get_json()
+    user_json = request.get_json()
     response_object = {"status": "fail", "message": "Invalid payload"}
-    if not post_data:
+    
+    if not user_json:
         return jsonify(response_object), 400
 
-    username = post_data.get("username")
-    mail = post_data.get("mail")
+    username = user_json.get("username")
+    mail = user_json.get("mail")
    
     try:
         user = User.query.filter(
-            or_(User.username == username, User.mail == mail)
+            (User.username == username) | (User.mail == mail)
         ).first()
+        
         if not user:
             # add new user to db
-            user_json = request.get_json()
-
             user = UserSchema().load(user_json)
             db.session.add(user)
             db.session.commit()
-            new_user = User.query.filter(mail=mail)
+            
+            #Check if user was correctly generated
+            user = User.query.filter_by(mail=mail).first()
+            
+            print(f"\n\nTHIS IS THE USER {user}\n\n")
+            print("\n\nMAKING ATTEMPT\n\n")
+            
             # generate auth token
-            auth_token = new_user.encode_auth_token(new_user.id)
+            auth_token = generate_auth_token(user.public_id)
+            
             response_object["status"] = "success"
             response_object["message"] = "Successfully registered."
-            response_object["auth_token"] = auth_token.decode()
+            response_object["auth_token"] = auth_token
+            
             return jsonify(response_object), 200
         else:
             response_object["message"] = "Sorry. That user already exists."
             return jsonify(response_object), 400
+        
     except exc.IntegrityError as e:
         db.session.rollback()
         log.error(e)
         return jsonify(response_object), 400
+    
     except ValueError as e:
         db.session.rollback()
         log.error(e)
