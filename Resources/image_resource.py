3c46c6326903cb9450234003135263049867b828
Ulises E. Sosa <ulyses.sosa@gmail.com>
2026-01-25 18:46:42 +0100

    image as a blueprint

--- Resources/image_resource.py
+++ Resources/image_resource.py
@@ -1,88 +1,56 @@
+from http import HTTPStatus
 import logging
-from flask_restful import Resource, abort
-from sqlalchemy.orm.exc import NoResultFound
+from flask import Blueprint, jsonify, abort
+from werkzeug.exceptions import HTTPException
+from sqlalchemy.exc import IntegrityError
 from Schemas.image_schema import ImageSchema 
 from Models.image import Image
-from flask import request
-from sqlalchemy.exc import IntegrityError
 from Api.database import db
+from Services.Middleware.auth_middleware import JWTService
+from Services.Middleware.privileges_middleware import require_owner
 
 logger = logging.getLogger(__name__) # It will print the name of this module when the main app is running
 
 IMAGE_ENDPOINT = "/api/image"
+image_api = Blueprint('image', __name__, url_prefix=IMAGE_ENDPOINT)
 
-class ImageResource(Resource):
+@image_api.route("/<int:id>", methods=["GET"])
+@JWTService.authenticate_restful
+@require_owner('image')
+def get_image(id: int):
+    """GET /api/image/<id> - Retrieve image by ID"""
+    image = db.session.get(Image, id)
 
-    def __retrieve_image_by_id(self,id):
-        return Image.query.filter_by(id = id).first()
-    
-    def get(self, id=None):
-        """
-        ImageResource GET method. Retrieves the information related to the image with the passed id in the request
-        """
-         
-        try:
-            id = request.args.get('id')
-            image = self.__retrieve_image_by_id(id)
-            image_json = ImageSchema().dump(image)
-            if not image_json:
-                raise NoResultFound()
-            return image_json,200
-        except NoResultFound:
-                abort(404, message="No images available")
-        except Exception as e:
-            abort(500, message=f"Error:{e}")
-    
-    def post(self):
-        """
-        ImageResource POST method. Adds a new image to the database.
+    if not image:
+        abort(HTTPStatus.NOT_FOUND, description=f"Image with id {id} not found")
 
-        :return: Image, 201 HTTP status code.
-        """
+    return jsonify(ImageSchema().dump(image)), HTTPStatus.OK
 
-        try:
-            image = ImageSchema().load(request.get_json())
-            db.session.add(image)
-            db.session.commit()
-            image = self.__retrieve_image_by_id(image.id)
-            return ImageSchema().dump(image), 201
+@image_api.route("/<int:id>", methods=["DELETE"])
+@JWTService.authenticate_restful
+@require_owner('image')
+def delete_image(id: int):
+    """DELETE /api/image/<id> - Delete image by ID"""
+    try:
+        image = db.session.get(Image, id)
         
-        except IntegrityError as e:
-            db.session.rollback()
-            logger.error(
-                f"Error: {e}"
-            )
-
-            abort(500, message="Unexpected Error!")
+        if not image:
+            abort(HTTPStatus.NOT_FOUND, description=f"Image with id {id} not found")
         
-    def put(self):
-        try:
-            logger.info(f"Update image {request.get_json()} in db")
-            image = ImageSchema().load(request.get_json())
-            db.session.merge(image)
-            db.session.commit()
-            image = self.__retrieve_image_by_id(image.id)
-            return ImageSchema().dump(image), 201
-
-        except Exception as e:
-            logger.error(
-                f"Error: {e}"
-            )
-            db.session.rollback()
-            abort(500, message="Error: {e}")
-
-    def delete(self):
-        try:
-            image_id = request.args.get('id')
-            logger.info(f"Deleting image {image_id} ")
-
-            image = self.__retrieve_image_by_id(image_id)
-            db.session.delete(image)
-            db.session.commit()
-            logger.info(f"Image with id {image_id} successfully deleted")
-            return "Deletion successful", 200
-
-        except Exception as e:
-            db.session.rollback()
-            abort(
-                500, message=f"Error: {e}")
+        db.session.delete(image)
+        db.session.commit()
+        logger.info(f"Image with id {id} successfully deleted")
+        
+        return jsonify({"message": "Deletion successful"}), HTTPStatus.OK
+        
+    except HTTPException:
+        # Re-raise HTTP exceptions to let global handler format them
+        raise
+    except IntegrityError as e:
+        logger.error(f"Integrity error deleting image {id}: {e}")
+        db.session.rollback()
+        abort(HTTPStatus.CONFLICT, description="Cannot delete image due to existing references")
+    except Exception as e:
+        logger.error(f"Error deleting image {id}: {e}")
+        db.session.rollback()
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(e))
