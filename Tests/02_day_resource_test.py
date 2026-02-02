50f6a669cd2021e95440fbbf54edba537978b8c1
Ulises E. Sosa <ulyses.sosa@gmail.com>
2026-02-02 12:18:53 +0100

    fix namings for tests

--- Tests/02_day_resource_test.py
+++ Tests/02_day_resource_test.py
@@ -5,7 +5,7 @@ from datetime import date, datetime
 from Schemas.day_schema import DaySchema
 import json
 
-def test_insert_itinerary_with_day(client):
+def test_insert_itinerary_with_day(client, auth_headers):
     daysList = [
         {
             "day_number": 1,
@@ -17,11 +17,11 @@ def test_insert_itinerary_with_day(client):
         "days": daysList,
         "trip_id": 1
     }
-    response = client.post(ITINERARY_ENDPOINT, json=itinerary)
+    response = client.post(ITINERARY_ENDPOINT, json=itinerary, headers=auth_headers)
     assert response.status_code == 201
 
 
-def test_insert_weather_to_day(client):
+def test_insert_weather_to_day(client, auth_headers):
     weather = {
         "weather_states": [],
         "day_id": 1,
@@ -32,11 +32,11 @@ def test_insert_weather_to_day(client):
         "id": 1,
         "weather": weather
     }
-    response = client.post(f"{DAY_ENDPOINT}/update", json=day)
+    response = client.post(f"{DAY_ENDPOINT}/update", json=day, headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
 
 
-def test_insert_sea_to_day(client):
+def test_insert_sea_to_day(client, auth_headers):
     sea = {
         "sea_states": [],
         "day_id": 1,
@@ -49,11 +49,11 @@ def test_insert_sea_to_day(client):
         "itinerary_id":1,
         "sea": sea
     }
-    response = client.post(f"{DAY_ENDPOINT}/update", json=day)
+    response = client.post(f"{DAY_ENDPOINT}/update", json=day, headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
 
 
-def test_get_day_by_key(client):
+def test_get_day_by_key(client, auth_headers):
     day_date = date.fromisoformat('2020-12-31').strftime("%Y-%m-%d")
     json = {
         "itinerary_id":1,
@@ -62,41 +62,41 @@ def test_get_day_by_key(client):
     }
 
     response = client.post(
-        f"{DAY_ENDPOINT}/by-key", json=json)
+        f"{DAY_ENDPOINT}/by-key", json=json, headers=auth_headers)
     assert response.status_code == 200
 
-def test_get_day_by_id(client):
+def test_get_day_by_id(client, auth_headers):
     json_data = {
         "ids": [1]
     }
     response = client.post(
-        f"{DAY_ENDPOINT}/read_by_ids", json=json_data)
+        f"{DAY_ENDPOINT}/read_by_ids", json=json_data, headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
 
-def test_get_all_days(client):
+def test_get_all_days(client, auth_headers):
     response = client.get(
-        f"{DAY_ENDPOINT}/itinerary/1")
+        f"{DAY_ENDPOINT}/itinerary/1", headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
     assert len(json.loads(response.data)) == 1
 
-def test_update_day(client):
+def test_update_day(client, auth_headers):
     day_date = date.fromisoformat('2020-12-31').strftime("%Y-%m-%d")
     sea = None
     updated_day = {'id':1,'date':day_date,'itinerary_id':1,'day_number':1, 'sea':sea} 
-    response = client.post(f"{DAY_ENDPOINT}/update", json=updated_day)
+    response = client.post(f"{DAY_ENDPOINT}/update", json=updated_day, headers=auth_headers)
     json_data = json.loads(response.data)
     print("json data ",json_data)
     assert json_data['sea'] == sea
     assert response.status_code == HTTPStatus.OK
 
-def test_delete_day_by_id(client):
+def test_delete_day_by_id(client, auth_headers):
     response = client.delete(
-        f"{DAY_ENDPOINT}/1")
+        f"{DAY_ENDPOINT}/1", headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
 
-def test_reinsert_day(client):
+def test_reinsert_day(client, auth_headers):
     day_date = date.fromisoformat('2020-12-31').strftime("%Y-%m-%d")
     new_day = {'id':1,'date':day_date,'itinerary_id':1,'day_number':1} 
-    response = client.post(f"{DAY_ENDPOINT}/create", json=new_day)
+    response = client.post(f"{DAY_ENDPOINT}/create", json=new_day, headers=auth_headers)
 
     assert response.status_code == HTTPStatus.CREATED
